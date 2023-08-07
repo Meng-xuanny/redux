79a807e5e4ee1264e6cefc80bbcec6fb53311192
Mengxuan Liang <liangmengxuan1123@gmail.com>
2023-08-07 14:52:36 +1000

    redux toolkit project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+
+function App() {
+  const dispatch = useDispatch();
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems("hello"));
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
